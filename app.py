396204d298e8f5f2484ae55d6ad27c6926ee6b4a
Antoine Guyon <antoineguyon@FRDIJALT0010.local>
2024-05-14 10:46:22 +0200

    add cors feature

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/records/<frmannee>/<frmclub>/<frmepreuve>')
 def get_records(frmannee, frmclub, frmepreuve):
